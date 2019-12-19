4d8d5884f5230517963a95f125525d326de57b19
Jim Fitzpatrick <jimfity@gmail.com>
2019-12-19 09:54:15 +0000

    Feat(Favourites): Filter feature added to over all list view

--- app/src/main/java/org/wit/hillfort/views/hillfortlist/HillfortListPresenter.kt
+++ app/src/main/java/org/wit/hillfort/views/hillfortlist/HillfortListPresenter.kt
@@ -1,7 +1,11 @@
 package org.wit.hillfort.views.hillfortlist
 
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_hillfort_list.*
 import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.startActivityForResult
+import org.wit.hillfort.R
 import org.wit.hillfort.helpers.BLANK_REQUEST
 import org.wit.hillfort.helpers.EDIT
 import org.wit.hillfort.main.MainApp
@@ -26,4 +30,48 @@ class HillfortListPresenter(val view: HillfortListView) {
         view.startActivityForResult(view.intentFor<EditHillfortView>().putExtra(EDIT, hillfort), BLANK_REQUEST)
     }
 
+    fun doOnOptionsItemSelected(item: MenuItem?) {
+        when (item?.itemId) {
+            R.id.item_add -> doAddHillfort()
+            R.id.show_fav -> showFavourites()
+            R.id.hide_fav -> displayAll()
+        }
+    }
+    fun doFilterHillfortList(filter: Boolean){
+        if (filter){
+            setupHillfortsListView(getHillforts().filter { it.favourite })
+
+        } else {
+            setupHillfortsListView(getHillforts())
+        }
+    }
+
+    private fun showFavourites() {
+        changeStarIcon(true)
+        doFilterHillfortList(true)
+    }
+
+    private fun displayAll() {
+       changeStarIcon(false)
+        doFilterHillfortList(false)
+    }
+
+    private fun changeStarIcon(color: Boolean) {
+        val showFav = view.menu.findItem(R.id.show_fav)
+        val hideFav = view.menu.findItem(R.id.hide_fav)
+        if (color) {
+            showFav.isVisible = false
+            hideFav.isVisible = true
+        } else {
+            showFav.isVisible = true
+            hideFav.isVisible = false
+        }
+    }
+
+    private fun setupHillfortsListView(hillfort: List<HillfortModel>){
+        val layoutManager = LinearLayoutManager(view)
+        view.recyclerView.layoutManager = layoutManager
+        view.recyclerView.adapter = HillfortAdapter(hillfort, view)
+        view.recyclerView.adapter?.notifyDataSetChanged()
+    }
 }
